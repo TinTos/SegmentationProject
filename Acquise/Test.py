Fixing stride bug in RectTileSet.py, introducing 0 to 1 scaling and using custom scaling of source image

Acquise/Test.py
@@ -36,6 +36,18 @@ def update_viewer():
 
 
 def get_data():
+    min_contrast = viewer.layers[0].contrast_limits[0]
+    max_contrast = viewer.layers[0].contrast_limits[1]
+    ovrscaled = ovr.copy()
+    ovrscaled[ovrscaled < min_contrast] = min_contrast
+    ovrscaled[ovrscaled > max_contrast] = max_contrast
+    ovrscaled -= min_contrast
+    ovrscaled /= (max_contrast - min_contrast)
+
+    min_debug = np.min(ovrscaled)
+    max_debug = np.max(ovrscaled)
+
+
     rects = []
     labels = []
     for i in range(1, len(viewer.layers)):
@@ -65,7 +77,7 @@ def get_data():
     del dstrain
     del dsval
 
-    ds2 = TileDataset2(ovr, 64)
+    ds2 = TileDataset2(ovrscaled, 64)
 
     inference_batchsize = 256
     inference_batchsize_found = False
@@ -73,7 +85,7 @@ def get_data():
         #try:
     torch.cuda.empty_cache()
     dl2 = torch.utils.data.DataLoader(ds2, batch_size=int(inference_batchsize))
-    inferred = inference_routine(model_ft, dl2, ovr, 64)
+    inferred = inference_routine(model_ft, dl2, ovrscaled, 64)
     inference_batchsize_found = True
         #except:
             #print("OS error: {0}".format(err))
@@ -112,11 +124,13 @@ if __name__ =='__main__':
     if app == None:
         app = QApplication([])
 
-    ovr=np.load('Outputdata/overview.npy')[0:6000,0:6000]
-    lb=np.load('Outputdata/lable.npy')
+
+    ovr=np.load('Outputdata/overview.npy')[0:4000,0:4000]
+
+    #lb=np.load('Outputdata/lable.npy')
     #image_resized = resize(ovr, (ovr.shape[0] // 16, ovr.shape[1] // 16), anti_aliasing=True)
 
-    lbrs = np.load('Outputdata/label_2000_4000_fullres.npy')
+    #lbrs = np.load('Outputdata/label_2000_4000_fullres.npy')
     #lbrs = resize_large(lb, ovr.shape)
 
     viewer = napari.view_image(ovr, rgb=False)

TorchLearning/PretrainedModel.py
@@ -15,10 +15,18 @@ import copy
 def get_pretrained_model_criterion_optimizer_scheduler():
     model_ft = models.resnet18(pretrained=True)
     #model_ft.classifier = nn.Sequential(nn.Dropout(p=0.2, inplace=False), nn.Linear(in_features=1280, out_features=2, bias=True))
-    num_ftrs = model_ft.fc.in_features
+    #num_ftrs = model_ft.fc.in_features
     # Here the size of each output sample is set to 2.
     # Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
-    model_ft.fc = nn.Linear(num_ftrs, 2)
+    #model_ft.fc = nn.Linear(num_ftrs, 2)
+
+    for param in model_ft.parameters():
+        param.requires_grad = False
+
+    # Parameters of newly constructed modules have requires_grad=True by default
+    num_ftrs = model_ft.fc.in_features
+    model_ft.fc = nn.Linear(num_ftrs, 100)
+    model_ft = nn.Sequential(model_ft, nn.ReLU(), nn.Linear(100,2))
 
     criterion = nn.BCEWithLogitsLoss()
 
@@ -31,7 +39,7 @@ def get_pretrained_model_criterion_optimizer_scheduler():
     return model_ft.cuda(), criterion.cuda(), optimizer_ft, exp_lr_scheduler
 
 
-def train_model(model, criterion, optimizer, scheduler, dataloaders, isRGB = False, num_epochs=5):
+def train_model(model, criterion, optimizer, scheduler, dataloaders, isRGB = False, num_epochs=7):
     since = time.time()
 
     best_model_wts = copy.deepcopy(model.state_dict())
@@ -67,7 +75,13 @@ def train_model(model, criterion, optimizer, scheduler, dataloaders, isRGB = Fal
                 # zero the parameter gradients
                 optimizer.zero_grad()
 
-                if not isRGB: inputs = torch.cat([inputs,inputs,inputs], dim = 1)
+                #if not isRGB: inputs = torch.cat([inputs,inputs,inputs], dim = 1)
+                if not isRGB: inputs = inputs.repeat(1,3,1,1)
+                inputs = inputs - inputs.view(inputs.shape[0],3,inputs.shape[-1]*inputs.shape[-1]).min(axis=2)[0].reshape(inputs.shape[0],3,1,1)
+                inputs = inputs / inputs.view(inputs.shape[0],3,inputs.shape[-1]*inputs.shape[-1]).max(axis=2)[0].reshape(inputs.shape[0],3,1,1)
+
+                #print(inputs[0])
+
                 inputs = F.interpolate(inputs, size=224)
                 inputs = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])(inputs)
 
@@ -84,7 +98,7 @@ def train_model(model, criterion, optimizer, scheduler, dataloaders, isRGB = Fal
                     if phase == 'train':
                         loss.backward()
                         optimizer.step()
-                        #scheduler.step(loss)
+                        scheduler.step(loss)
 
                 # statistics
                 running_loss += loss.item() * inputs.size(0)

TorchLearning/TestTraining.py
@@ -35,31 +35,39 @@ def training_routine(net, dataloader):
     print('Finished Training')
 
 
-def inference_routine(net, dataloader, overview, tilesize):
+def inference_routine(net, dataloader, overview, tilesize, isRGB = False):
+    min_debug = np.min(overview)
+    max_debug = np.max(overview)
+
     with torch.no_grad():
         if (len(overview.shape) == 2):
             overview = overview.reshape((1, overview.shape[0], overview.shape[1]))
 
         tilecounty = int(overview.shape[1] // tilesize)
         tilecountx = int(overview.shape[2] // tilesize)
-        result = np.zeros((tilecounty, tilecountx))
+        result = np.zeros((tilecounty*tilesize, tilecountx*tilesize))
 
         net.eval()
 
         for i, data in enumerate(dataloader):
             inputs, inds = data
 
-            inputs = torch.cat([inputs, inputs, inputs], dim=1)
+            if not isRGB: inputs = inputs.repeat(1,3,1,1)
+            inputs = inputs - inputs.view(inputs.shape[0],3,inputs.shape[-1]*inputs.shape[-1]).min(axis=2)[0].reshape(inputs.shape[0],3,1,1)
+            inputs = inputs / inputs.view(inputs.shape[0],3,inputs.shape[-1]*inputs.shape[-1]).max(axis=2)[0].reshape(inputs.shape[0],3,1,1)
+
             inputs = F.interpolate(inputs, size=224)
             inputs = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])(inputs)
 
             outputs = torch.sigmoid(net(inputs)).cpu().numpy()
 
-            #for count in range(inds[0].shape[0]):
-            result[inds[0][:],inds[1][:]] = outputs[:,0]
+            for count in range(inds[0].shape[0]):
+                result[inds[0][count] * tilesize : (inds[0][count] + 1) * tilesize, inds[1][count] * tilesize : (inds[1][count] + 1) * tilesize] = outputs[count,0]
 
             del inputs
             del outputs
+
+            print(str(i) + "/" + str(int(len(dataloader.dataset) // dataloader.batch_size)))
 
         return result
 