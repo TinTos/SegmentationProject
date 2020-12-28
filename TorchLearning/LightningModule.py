9e86b3c5eafbb621beb90b406522286e4ec7d6f5
MTosstorff <tosstorff@till-id.com>
2020-12-28 02:08:33 +0100

    Fixing LightningModule.py

--- TorchLearning/LightningModule.py
+++ TorchLearning/LightningModule.py
@@ -12,20 +12,21 @@ class LitModel(LightningModule):
         num_ftrs = self.classifier.fc.in_features
         self.classifier.fc = nn.Linear(num_ftrs, num_classes)
         self.learning_rate = learning_rate
+        self.lossobject = nn.BCEWithLogitsLoss()
 
     def forward(self, x):
         x = self.classifier(x)
         return x
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=(self.lr or self.learning_rate))
+        optimizer = torch.optim.Adam(self.parameters(), lr= self.learning_rate)
         return optimizer
 
     def training_step(self, batch, batch_idx, isRGB = False):
         x, y = batch
         x = preprocess(x, isRGB)
         outputs = self(x)
-        loss = nn.BCEWithLogitsLoss(outputs, y)
+        loss = self.lossobject(outputs, y)
 
         return loss
 
@@ -35,9 +36,12 @@ class LitModel(LightningModule):
         outputs = self(x)
         _, preds = torch.max(outputs, 1)
         _, comparisonlabel = torch.max(y, 1)
-        val_loss = torch.sum(preds == comparisonlabel) / batch.shape[0]
+        val_loss = torch.sum(preds == comparisonlabel) / len(batch)
 
         return val_loss
+        #loss = nn.BCEWithLogitsLoss()(outputs, y)
+
+        #return loss
 
 
     def test_step(self, batch, batch_idx, isRGB = False):
