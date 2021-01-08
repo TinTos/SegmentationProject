b2d34f209a665effc12c124051197f57e40d830e
MTosstorff <tosstorff@till-id.com>
2021-01-08 22:07:32 +0100

    Making adaptive shiftcount accessible in SegmentationRoutine.py

--- Colab/SegmentationRoutine.py
+++ Colab/SegmentationRoutine.py
@@ -8,10 +8,10 @@ from TorchLearning.LightningModule import LitModel
 from TorchLearning.TestTraining import inference_routine
 
 
-def segment(overview, polys, labels, tilesize=64, shiftcount=3, coveringthreshold=0.75, epochs = 6, model = None, labelundecisive = False, decisionthresh = 0.5):
+def segment(overview, polys, labels, tilesize=64, shiftcount=3, coveringthreshold=0.75, epochs = 6, model = None, labelundecisive = False, decisionthresh = 0.5, adaptiveshiftcount = False):
     numclasses = len(np.unique(labels))
 
-    dstrain, dsval = PolyDataset.from_scratch(overview, polys, labels, tilesize, shiftcount, coveringthreshold).split(0.1)
+    dstrain, dsval = PolyDataset.from_scratch(overview, polys, labels, tilesize, shiftcount, coveringthreshold).split(0.1) if not adaptiveshiftcount else PolyDataset.from_scratch_with_adapted_shiftcount(overview, polys, labels, tilesize, shiftcount, coveringthreshold).split(0.1)
 
     dltrain = torch.utils.data.DataLoader(dstrain, batch_size=64, shuffle=True)
     dlval = torch.utils.data.DataLoader(dsval, batch_size=64, shuffle=True)
