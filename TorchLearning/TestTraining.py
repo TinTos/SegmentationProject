868fb98ce969a9b48dcfb0ab828ad8ef4d458989
MTosstorff <tosstorff@till-id.com>
2021-01-10 23:54:18 +0100

    Introducing clustering for unsupervised learning

--- TorchLearning/TestTraining.py
+++ TorchLearning/TestTraining.py
@@ -4,6 +4,8 @@ import torch
 import numpy as np
 from TorchLearning.PretrainedModel import preprocess
 from TorchLearning.PretrainedModel import preprocess_for_custom
+from sklearn.cluster import KMeans
+
 def training_routine(net, dataloader):
     criterion = nn.BCELoss()
     optimizer = optim.Adam(net.parameters(), lr=0.001)
@@ -67,8 +69,7 @@ def inference_routine(net, dataloader, overview, tilesize, labelundecisive = Fal
         return result
 
 
-def cluster_routine(net, dataloader, overview, tilesize, num_features):
-
+def cluster_routine(net, dataloader, overview, tilesize, num_features, num_clusters):
     with torch.no_grad():
         if (len(overview.shape) == 2):
             overview = overview.reshape((1, overview.shape[0], overview.shape[1]))
@@ -77,8 +78,8 @@ def cluster_routine(net, dataloader, overview, tilesize, num_features):
 
         tilecounty = int(overview.shape[1] // tilesize)
         tilecountx = int(overview.shape[2] // tilesize)
-        result = np.zeros((num_features,tilecounty*tilesize, tilecountx*tilesize))
 
+        result = np.zeros((tilecounty * tilecountx,num_features))
         net.eval()
 
         for i, data in enumerate(dataloader):
@@ -86,10 +87,73 @@ def cluster_routine(net, dataloader, overview, tilesize, num_features):
 
             inputs = preprocess(inputs, isRGB)
 
-            outputs = torch.sigmoid(net(inputs)).cpu().numpy()
+            outputs = net(inputs).cpu().numpy()
+
+            outputs[np.isnan(outputs)] = 0
 
             for count in range(inds[0].shape[0]):
-                result[:,inds[0][count] * tilesize : (inds[0][count] + 1) * tilesize, inds[1][count] * tilesize : (inds[1][count] + 1) * tilesize] = outputs[count]
+                result[inds[0][count] * tilecountx + inds[1][count]] = outputs[count]
+
+            del inputs
+            del outputs
+
+            print(str(i) + "/" + str(int(len(dataloader.dataset) // dataloader.batch_size)))
+
+        kmeans = KMeans(n_clusters=num_clusters, random_state=0).fit(result)
+        traf = KMeans(n_clusters=num_clusters, random_state=0).fit_transform(result)
+        dists = traf[range(result.shape[0]), kmeans.labels_]
+        r = np.zeros((dists.shape)).astype(np.bool)
+        for l in np.unique(kmeans.labels_):
+            #r[kmeans.labels_ == l] = dists[kmeans.labels_ == l] - np.mean(dists[kmeans.labels_ == l]) < 0
+            r[kmeans.labels_ == l] = True
+
+
+        labeledim = np.zeros((tilecounty * tilesize, tilecountx * tilesize))
+        for i in range(len(r)):
+            indices = (int(i // tilecountx), int(i % tilecountx))
+            if r[i] == True: labeledim[indices[0] * tilesize : (indices[0] + 1) * tilesize, indices[1] * tilesize : (indices[1] + 1) * tilesize] = kmeans.labels_[i]
+
+
+
+
+        return labeledim
+
+
+
+from skimage.feature import local_binary_pattern
+
+
+def cluster_routine_2(dataloader, overview, tilesize, num_features, num_clusters):
+    with torch.no_grad():
+        if (len(overview.shape) == 2):
+            overview = overview.reshape((1, overview.shape[0], overview.shape[1]))
+
+        isRGB = overview.shape[0] == 3
+
+        tilecounty = int(overview.shape[1] // tilesize)
+        tilecountx = int(overview.shape[2] // tilesize)
+
+        num_features = tilesize * tilesize
+
+        result = np.zeros((tilecounty * tilecountx,num_features))
+
+
+        for i, data in enumerate(dataloader):
+            inputs, inds = data
+
+            #inputs = preprocess(inputs, isRGB).cpu().numpy()
+
+            inputs = inputs.cpu().numpy()
+            outputs = None
+
+            for i in range(inputs.shape[0]):
+                tmp = local_binary_pattern(inputs[i,0], 24, 3, 'uniform').reshape((tilesize * tilesize))
+                if outputs is None:
+                    outputs = np.zeros((inputs.shape[0], tmp.shape[0]))
+                outputs[i] = tmp
+
+            for count in range(inds[0].shape[0]):
+                result[inds[0][count] * tilecountx + inds[1][count]] = outputs[count]
 
             del inputs
             del outputs
@@ -97,9 +161,24 @@ def cluster_routine(net, dataloader, overview, tilesize, num_features):
             print(str(i) + "/" + str(int(len(dataloader.dataset) // dataloader.batch_size)))
 
 
+        kmeans = KMeans(n_clusters=num_clusters, random_state=0).fit(result)
+        traf = KMeans(n_clusters=num_clusters, random_state=0).fit_transform(result)
+        dists = traf[range(result.shape[0]), kmeans.labels_]
+        r = np.zeros((dists.shape)).astype(np.bool)
+        for l in np.unique(kmeans.labels_):
+            #r[kmeans.labels_ == l] = dists[kmeans.labels_ == l] - np.mean(dists[kmeans.labels_ == l]) < 0
+            r[kmeans.labels_ == l] = True
 
-        return result
 
+        labeledim = np.zeros((tilecounty, tilecountx))
+        for i in range(len(r)):
+            indices = (int(i // tilecountx), int(i % tilecountx))
+            if r[i] == True: labeledim[indices[0], indices[1]] = kmeans.labels_[i]
+
+
+
+
+        return labeledim
 
 
 
